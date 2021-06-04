1d3a129df42763ef48f793439f606b0b05cc415c
eragonweb <nagy.benceadam@gmail.com>
2021-06-04 11:09:31 +0200

    V3.2

--- src/main/java/com/example/beadando/car/CarService.java
+++ src/main/java/com/example/beadando/car/CarService.java
@@ -1,15 +1,17 @@
 package com.example.beadando.car;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class CarService {
-
-
+    @Autowired
+    private EntityManager entityManager;
     //TODO nem best practice
     private List<CarEntity> list = new ArrayList<>();
 
@@ -25,27 +27,29 @@ public class CarService {
             list.add(entity);
         }
     }
-    public List<CarEntity> findAll(){
+
+    public List<CarEntity> findAll() {
         return list;
     }
 
-    public CarEntity create(CarEntity entity){
+    public CarEntity create(CarEntity entity) {
         list.add(entity);
         return entity;
     }
-public  boolean deteteById(Long id){
-    for (CarEntity entity : list) {
-        if (entity.getId().equals(id)){
-            list.remove(entity);
-            return true;
-        }
-    }
-    return false;
-}
 
-    public CarEntity update(CarEntity entity){
-        CarEntity updateCar= findCarById(entity.getId());
-        if(updateCar!=null){
+    public boolean deteteById(Long id) {
+        for (CarEntity entity : list) {
+            if (entity.getId().equals(id)) {
+                list.remove(entity);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public CarEntity update(CarEntity entity) {
+        CarEntity updateCar = findCarById(entity.getId());
+        if (updateCar != null) {
             updateCar.setType(entity.getType());
             updateCar.setDoor_number(entity.getDoor_number());
             updateCar.setManufacturer(entity.getManufacturer());
@@ -55,6 +59,7 @@ public  boolean deteteById(Long id){
 
         return updateCar;
     }
+
     public CarEntity findCarById(Long id) {
         for (CarEntity entity : list) {
             if (entity.getId().equals(id)) {
