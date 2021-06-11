e56ef70eaa7209b96691d7ff8ff2eba8efe0d6e3
eragonweb <nagy.benceadam@gmail.com>
2021-06-11 15:52:49 +0200

    V6.6 de a szamok nem textField emaitt az még nem megy

---
+++ src/main/java/com/example/beadando/car/view/CarManagerView.java
@@ -0,0 +1,118 @@
+package com.example.beadando.car.view;
+import com.example.beadando.car.entity.CarEntity;
+import com.example.beadando.car.service.CarService;
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.router.Route;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+import javax.annotation.PostConstruct;
+//http://localhost:8081/carmanager
+@Route
+public class CarManagerView extends VerticalLayout{
+    private CarEntity selectedCarEntity;
+    private VerticalLayout form;
+    private TextField type;
+    private TextField manufacturer;
+    private TextField door_number;
+    private TextField manufacturer_year;
+
+
+
+    private Binder<CarEntity> binder;
+    @Autowired
+    private CarService service;
+
+    @PostConstruct
+    public void init() {
+        Grid<CarEntity> grid = new Grid<>();
+        grid.setItems(service.findAll());
+        grid.addColumn(CarEntity::getID).setHeader("ID");
+        grid.addColumn(CarEntity::getType).setHeader("Type");
+        grid.addColumn(CarEntity::getManufacturer).setHeader("Manufacturer");
+        grid.addColumn(CarEntity::getDoor_number).setHeader("Door Number");
+        grid.addColumn(CarEntity::getManufacturer_year).setHeader("Manufacturer_year");
+
+        addButtonBar(grid);
+        add(grid);
+        addForm(grid);
+    }
+
+    private void addForm(Grid<CarEntity> grid) {
+        form = new VerticalLayout();
+        binder = new Binder<>(CarEntity.class);
+        type = new TextField();
+        manufacturer = new TextField();
+        door_number= new TextField();
+        manufacturer_year= new TextField();
+        form.add(new Text("Type"),type);
+        form.add(new Text("Manufacturer Name"), manufacturer);
+        form.add(new Text("Door Number"),door_number);
+        form.add(new Text("Manufactured year"), manufacturer_year);
+
+
+
+        Button saveBtn = new Button();
+        saveBtn.setText("Save");
+        saveBtn.addClickListener(buttonClickEvent -> {
+            if (selectedCarEntity.getID() != null) {
+                service.update(selectedCarEntity);
+            } else {
+                service.create(selectedCarEntity);
+            }
+            grid.setItems(service.findAll());
+            form.setVisible(false);
+        });
+        form.add(saveBtn);
+        add(form);
+        form.setVisible(false);
+        binder.bindInstanceFields(this);
+
+    }
+
+    private void addButtonBar(Grid<CarEntity> grid) {
+        HorizontalLayout buttonBar = new HorizontalLayout();
+
+        Button deleteBTN = new Button();
+        deleteBTN.setEnabled(false);
+        deleteBTN.setText("Delete");
+        deleteBTN.setIcon(VaadinIcon.TRASH.create());
+        deleteBTN.addClickListener(buttonClickEvent -> {
+            service.deteteById(selectedCarEntity.getID());
+            grid.setItems(service.findAll());
+            selectedCarEntity = null;
+            deleteBTN.setEnabled(false);
+            form.setVisible(false);
+            Notification.show("Succesfully Deleted");
+        });
+
+        grid.asSingleSelect().addValueChangeListener(event -> {
+            selectedCarEntity = event.getValue();
+            deleteBTN.setEnabled(selectedCarEntity != null);
+            form.setVisible(selectedCarEntity != null);
+            binder.setBean(selectedCarEntity);
+
+
+        });
+
+        Button addBtn = new Button();
+        addBtn.setText("Add");
+        addBtn.addClickListener(buttonClickEvent -> {
+            selectedCarEntity =new CarEntity();
+            binder.setBean(selectedCarEntity);
+            form.setVisible(true);
+        });
+        addBtn.setIcon(VaadinIcon.PLUS.create());
+        buttonBar.add(deleteBTN, addBtn);
+        add(buttonBar);
+
+    }
+}
---
+++ src/main/java/com/example/beadando/core/CoreEntity.java
@@ -0,0 +1,25 @@
+package com.example.beadando.core;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public abstract class CoreEntity {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
+
+
+
