42c23ac70af17584960779f73e40d1e73ee7640a
eragonweb <nagy.benceadam@gmail.com>
2021-06-11 11:34:24 +0200

    V6.4

--- src/main/java/com/example/beadando/manufacturer/view/ManufacturerManagerView.java
+++ src/main/java/com/example/beadando/manufacturer/view/ManufacturerManagerView.java
@@ -4,19 +4,24 @@ import com.example.beadando.manufacturer.entity.ManufacturerEntity;
 import com.example.beadando.manufacturer.service.ManufacturerService;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.PostConstruct;
-import java.util.ArrayList;
-import java.util.List;
+
 
 @Route
 public class ManufacturerManagerView extends VerticalLayout {
 private  ManufacturerEntity selectedManufacturer;
     @Autowired
     private ManufacturerService service;
+
+
+
     @PostConstruct
     public void init(){
         Grid<ManufacturerEntity> grid=new Grid<>();
@@ -24,24 +29,40 @@ private  ManufacturerEntity selectedManufacturer;
         grid.addColumn(ManufacturerEntity::getId).setHeader("ID");
         grid.addColumn(ManufacturerEntity::getName).setHeader("Neve");
 
+
+        add(grid);
+    }
+    private void addButtonBar(Grid<ManufacturerEntity> grid){
+        HorizontalLayout buttonBar=new HorizontalLayout();
         Button deleteBTN=new Button();
         deleteBTN.setEnabled(false);
         deleteBTN.setText("Törlés");
+        deleteBTN.setIcon(VaadinIcon.TRASH.create());
         deleteBTN.addClickListener(buttonClickEvent -> {
 
-           service.deteteById(selectedManufacturer.getId());
+            service.deteteById(selectedManufacturer.getId());
             grid.setItems(service.findAll());
             selectedManufacturer=null;
             deleteBTN.setEnabled(false);
+            Notification.show("Sikeres Törtlés");
         });
 
         grid.asSingleSelect().addValueChangeListener(event ->{
             selectedManufacturer=event.getValue();
             deleteBTN.setEnabled(selectedManufacturer!=null);
-        });
-        add(deleteBTN);
-        add(grid);
 
+        });
+
+        Button addBtn=new Button();
+        addBtn.setText("Hozzáadás");
+        addBtn.addClickListener(buttonClickEvent -> {
+            //TODO uj kerálása
+        } );
+        addBtn.setIcon(VaadinIcon.PLUS.create());
+        buttonBar.add(deleteBTN,addBtn);
+        add(buttonBar);
 
     }
+
+
 }
